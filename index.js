962c8ceba9309b6e3d3a7b5ad42d9f04ca902c3b
Eric Yi <erickanginyi@gmail.com>
2021-09-13 17:41:40 -0400

    Create more HTML, start to add JS DOM Manipulation

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-console.log("fuck yourself");   
+const BASE_URL = "http://localhost:3000/Characters";
+
+function receiveInformation() {
+    fetch(BASE_URL)
+    .then(resp => resp.json())
+    .then(data => data.forEach(character => renderCharacter(character)));
+}
+receiveInformation();
+
+function renderCharacter(character) {
+    const characterBar = document.querySelector("#character-bar");
+    
+    const barName = document.createElement("p");
+    barName.textContent = character.name;
+
+    const barImage = document.createElement("img");
+    barImage.src = character.image;
+    barImage.alt = character.name;
+    barImage.title = character.name;
+    barImage.width = 100;
+
+    characterBar.append(barName, barImage);
+    
+    barImage.addEventListener("click", e => characterBarClickEvent(character));
+}
+
+function characterBarClickEvent(character) {
+    
+}
+
+//Work on the functionality of this for your stat rolls button
+// const statButton = document.querySelector(".dice");
+// statButton.addEventListener("click", e => {
+//     statButton.remove();
+//     const showStats = document.querySelector(".stat-roll");
+
+//     const newP = document.createElement("p");
+//     newP.textContent = Math.floor(Math.random() * 20) + 1;
+
+//         showStats.appendChild(newP);
+
+// } )
