924c768d69fc30abda922b1801bc73f59b05b034
Eric Yi <erickanginyi@gmail.com>
2021-09-14 11:22:30 -0400

    Add roll values for character creation

--- index.js
+++ index.js
@@ -40,6 +40,24 @@ function characterBarClickEvent(character) {
 
     const characterClass = document.querySelector("#character-class");
     characterClass.textContent = `Character Class: ${character.class}`;
+
+    const charStat = document.querySelector("#char-stat");
+    charStat.textContent = `CHA: ${character.stats.charisma}`;
+
+    const conStat = document.querySelector("#con-stat");
+    conStat.textContent = `CON: ${character.stats.constitution}`;
+
+    const dexStat = document.querySelector("#dex-stat");
+    dexStat.textContent = `DEX: ${character.stats.dexterity}`;
+
+    const intStat = document.querySelector("#int-stat");
+    intStat.textContent = `INT: ${character.stats.intelligence}`;
+
+    const strStat = document.querySelector("#str-stat");
+    strStat.textContent = `STR: ${character.stats.strength}`;
+
+    const wisStat = document.querySelector("#wis-stat");
+    wisStat.textContent = `WIS: ${character.stats.wisdom}`;
 }
 
 const characterForm = document.querySelector('#character-form');
@@ -47,20 +65,32 @@ characterForm.addEventListener('submit', submitCharacter);
 
 function submitCharacter(event){
     event.preventDefault();
+    
+    // const showStatsList = document.querySelectorAll(".stat-roll");
+    // const showStats = Array.from(showStatsList);
+
     const newCharacter = {
         name: event.target['name-input'].value,
         image: event.target['img-input'].value,
         race: event.target['race-input'].value,
-        class: event.target['class-input'].value
+        class: event.target['class-input'].value,
+        stats: {
+            charisma: Math.floor(Math.random() * 20) + 1,
+            constitution: Math.floor(Math.random() * 20) + 1,
+            dexterity: Math.floor(Math.random() * 20) + 1,
+            intelligence: Math.floor(Math.random() * 20) + 1,
+            strength: Math.floor(Math.random() * 20) + 1,
+            wisdom: Math.floor(Math.random() * 20) + 1
+        }
     }
+
     renderCharacter(newCharacter);
+    characterBarClickEvent(newCharacter);
     event.target.reset();
 }
 
 //Work on the functionality of this for your stat rolls button
 
-//***************Essentially rollStats1*****************************************/
-//This rollStats function instantly randomly rolls for the 6 traits
 // function rollStats () {
 //     const statButton = document.querySelector("#dice");
 //         statButton.addEventListener("click", e => {
@@ -77,73 +107,82 @@ function submitCharacter(event){
 //         })
 // }
 // rollStats();
-//*****************************************************************************/
 
-function characterStats () {
-    const charButton = document.querySelector("#char-dice");
-    charButton.addEventListener("click", e => {
-        e.target.remove();
 
-        const charisma = document.querySelector("#char-roll");
+// function characterStats () {
+
+//     const charButton = document.querySelector("#char-dice");
+//     charButton.addEventListener("click", e => {
+//         e.target.remove();
+
+//         const charisma = document.querySelector("#char-roll");
         
-        const charRoll = document.createElement("p");
-        charRoll.textContent = Math.floor(Math.random() * 20) + 1;
-        charisma.appendChild(charRoll);
-    })
+//         const charRoll = document.createElement("p");
+//         charRoll.textContent = Math.floor(Math.random() * 20) + 1;
+//         charisma.appendChild(charRoll);
+//     })
 
-    const constButton = document.querySelector("#const-dice");
-    constButton.addEventListener("click", e => {
-        e.target.remove();
+//     const constButton = document.querySelector("#const-dice");
+//     constButton.addEventListener("click", e => {
+//         e.target.remove();
 
-        const constitution = document.querySelector("#const-roll");
+//         const constitution = document.querySelector("#const-roll");
         
-        const constRoll = document.createElement("p");
-        constRoll.textContent = Math.floor(Math.random() * 20) + 1;
-        constitution.appendChild(constRoll);
-    })
+//         const constRoll = document.createElement("p");
+//         constRoll.textContent = Math.floor(Math.random() * 20) + 1;
+//         constitution.appendChild(constRoll);
+//     })
 
-    const dexButton = document.querySelector("#dex-dice");
-    dexButton.addEventListener("click", e => {
-        e.target.remove();
+//     const dexButton = document.querySelector("#dex-dice");
+//     dexButton.addEventListener("click", e => {
+//         e.target.remove();
 
-        const dexterity = document.querySelector("#dex-roll");
+//         const dexterity = document.querySelector("#dex-roll");
         
-        const dexRoll = document.createElement("p");
-        dexRoll.textContent = Math.floor(Math.random() * 20) + 1;
-        dexterity.appendChild(dexRoll);
-    })
+//         const dexRoll = document.createElement("p");
+//         dexRoll.textContent = Math.floor(Math.random() * 20) + 1;
+//         dexterity.appendChild(dexRoll);
+//     })
 
-    const intButton = document.querySelector("#int-dice");
-    intButton.addEventListener("click", e => {
-        e.target.remove();
+//     const intButton = document.querySelector("#int-dice");
+//     intButton.addEventListener("click", e => {
+//         e.target.remove();
 
-        const intelligence = document.querySelector("#int-roll");
+//         const intelligence = document.querySelector("#int-roll");
         
-        const intRoll = document.createElement("p");
-        intRoll.textContent = Math.floor(Math.random() * 20) + 1;
-        intelligence.appendChild(intRoll);
-    })
+//         const intRoll = document.createElement("p");
+//         intRoll.textContent = Math.floor(Math.random() * 20) + 1;
+//         intelligence.appendChild(intRoll);
+//     })
 
-    const strButton = document.querySelector("#str-dice");
-    strButton.addEventListener("click", e => {
-        e.target.remove();
+//     const strButton = document.querySelector("#str-dice");
+//     strButton.addEventListener("click", e => {
+//         e.target.remove();
 
-        const strength = document.querySelector("#str-roll");
+//         const strength = document.querySelector("#str-roll");
         
-        const strRoll = document.createElement("p");
-        strRoll.textContent = Math.floor(Math.random() * 20) + 1;
-        strength.appendChild(strRoll);
-    })
+//         const strRoll = document.createElement("p");
+//         strRoll.textContent = Math.floor(Math.random() * 20) + 1;
+//         strength.appendChild(strRoll);
+//     })
 
-    const wisButton = document.querySelector("#wis-dice");
-    wisButton.addEventListener("click", e => {
-        e.target.remove();
+//     const wisButton = document.querySelector("#wis-dice");
+//     wisButton.addEventListener("click", e => {
+//         e.target.remove();
 
-        const wisdom = document.querySelector("#wis-roll");
+//         const wisdom = document.querySelector("#wis-roll");
         
-        const wisRoll = document.createElement("p");
-        wisRoll.textContent = Math.floor(Math.random() * 20) + 1;
-        wisdom.appendChild(wisRoll);
-    })
-}
-characterStats();
+//         const wisRoll = document.createElement("p");
+//         wisRoll.textContent = Math.floor(Math.random() * 20) + 1;
+//         wisdom.appendChild(wisRoll);
+//     })
+// }
+// characterStats();
+
+
+/**
+ * We're trying to add click event to the button
+ * Take showStats array and put that into db.json as the stat values
+ * We need this to turn into an object
+ * Need something to prevent them being able to create character without rolling stats first 
+ */
